d6dfed94eab94d1ed112cca0c6c3d1b4c74965e2
jeryckho <chery.jerome@free.fr>
2024-07-17 13:40:45 +0200

    gestion de fichiers

--- src/App.svelte
+++ src/App.svelte
@@ -8,8 +8,10 @@
 	import Edges from "./routes/Edges.svelte";
 	import Import from "./routes/Import.svelte";
 	import Graph from "./routes/Graph.svelte";
-	import { db } from "./stores/store";
+	import { db, inited, File } from "./stores/store";
 	import { Schema } from "./libs/queries";
+
+	let file = "";
 
 	const routes = {
 		"/": Home,
@@ -23,28 +25,69 @@
 
 	const Start = async () => {
 		try {
-			db.set(await Database.load("sqlite:test.db"));
+			const path = $File;
+			db.set(await Database.load(`sqlite://${path}`));
 			await $db.execute(Schema);
+			inited.set(true);
 		} catch (err) {
 			error(err);
 		}
+	};
 
-		// await db.close();
+	const Open = async () => {
+		try {
+			db.set(await Database.load(`sqlite://${file}`));
+			await $db.execute(Schema);
+			File.set(file);
+		} catch (err) {
+			error(err);
+		}
+	};
+
+	const Close = async () => {
+		await $db.close();
+		db.set(null);
 	};
 
 	Start();
 </script>
 
 <main>
-	<nav class="navbar is-light block">
-		<div class="navbar-brand">
-			<a class="navbar-item" href="#/"> Nodes </a>
-			<a class="navbar-item" href="#/Edges"> Edges </a>
-			<a class="navbar-item" href="#/Graph"> Graph </a>
-			<a class="navbar-item" href="#/Import"> Import </a>
-		</div>
-	</nav>
 	{#if $db}
+		<nav class="navbar is-light block">
+			<div class="navbar-brand">
+				<a class="navbar-item" href="#/"> Nodes </a>
+				<a class="navbar-item" href="#/Edges"> Edges </a>
+				<a class="navbar-item" href="#/Graph"> Graph </a>
+				<a class="navbar-item" href="#/Import"> Import </a>
+				<a class="navbar-item" href="#/"  on:click={Close}> Close </a>
+			</div>
+		</nav>
 		<Router {routes} />
+	{:else if $inited}
+		<nav class="navbar is-light block">
+			<div class="navbar-brand">
+				<a class="navbar-item" href="#/"> Open </a>
+			</div>
+		</nav>
+		<div class="columns">
+			<div class="column is-full">
+				<nav class="panel">
+					<p class="panel-heading">Open</p>
+					<div class="panel-block">
+						<div class="field has-addons">
+							<div class="control">
+							  <input class="input" type="text" bind:value={file} placeholder="enter file path">
+							</div>
+							<div class="control">
+							  <button class="button is-warning" on:click={Open}>
+								Connecter
+							  </button>
+							</div>
+						 </div>
+					</div>
+				</nav>
+			</div>
+		</div>
 	{/if}
 </main>
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,3 +1,15 @@
 import { writable } from 'svelte/store';
 
 export const db = writable(null);
+export const inited = writable(false);
+
+export const File = writable("strand");
+const Handle = (o) => {
+	for (const [key, value] of Object.entries(o)) {
+		const item = localStorage.getItem(key);
+		if (item) value.set(JSON.parse(item));
+		value.subscribe(item => { if (item) localStorage.setItem(key, JSON.stringify(item)) })
+	}
+}
+
+Handle({File});
